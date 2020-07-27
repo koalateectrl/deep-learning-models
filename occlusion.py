b1b94b6677599056abde79ab3e260a36a81be026
samwwong <sandymule@gmail.com>
2020-07-27 13:25:55 -0700

    fixed occulsion for bug

--- occlusion.py
+++ occlusion.py
@@ -5,7 +5,7 @@ import numpy as np
 
 def create_patch(image, x, y, patch_size):
 	patch_image = np.array(image, copy = True)
-	patch_image[x: x + patch_size, y: y + patch_size, :] = 127.5
+	patch_image[:, x: x + patch_size, y: y + patch_size, :] = 127.5
 	return patch_image
 
 
@@ -34,7 +34,7 @@ true_max_val = prediction[0, true_max_idx]
 
 print(true_max_val)
 
-patch_size = 32
+patch_size = 8
 occlusion_map = np.zeros((image.shape[1], image.shape[2]))
 
 #Run Occlusion by obscuring part of the image at a time
@@ -42,16 +42,10 @@ for x_val in range(0, image.shape[1], patch_size):
 	for y_val in range(0, image.shape[2], patch_size):
 		print(x_val, y_val)
 		patch_image = create_patch(image, x_val, y_val, patch_size)
-		occl_preds = test_step(patch_image, model)
+		occl_preds = test_step(patch_image, vgg_model)
 		occl_cls_prob = occl_preds[0, true_max_idx]
-		print(occl_cls_prob)
-
 		occlusion_map[x_val:x_val + patch_size, y_val: y_val + patch_size] = occl_cls_prob
 
-
-print(type(occlusion_map))
-print(occlusion_map.shape)
-print(occlusion_map)
 
 plt.imshow(occlusion_map)
 plt.show()
