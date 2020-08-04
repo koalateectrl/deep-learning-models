20b8fd6d29837a59192beb37b365cceb545b4fc5
samwwong <sandymule@gmail.com>
2020-08-04 00:25:03 -0700

    improved semi supervised gan

--- semi_supervised_gan.py
+++ semi_supervised_gan.py
@@ -4,7 +4,7 @@ import numpy as np
 
 
 # select a supervised subset of the dataset, all classes have same number of data points in the subset
-def select_supervised_samples(X_train, y_train, nb_samples = 100, nb_classes = 10):
+def select_supervised_samples(X_train, y_train, nb_samples = 1000, nb_classes = 10):
 	X_list, y_list = [], []
 	nb_per_class = int(nb_samples / nb_classes)
 	for class_num in range(nb_classes):
@@ -17,27 +17,10 @@ def select_supervised_samples(X_train, y_train, nb_samples = 100, nb_classes = 1
 		[y_list.append(class_num) for j in rand_instances]
 	return np.asarray(X_list), np.asarray(y_list)
 
-# select real samples
-def generate_real_samples(X_train, y_train, nb_samples):
-	# choose random instances
-	rand_instances = np.random.randint(0, X_train.shape[0], nb_samples)
-	# select images and labels
-	new_X, classes = X_train[rand_instances], y_train[rand_instances]
-
-	is_real = np.ones((nb_samples, 1))
-	return [new_X, classes], is_real
-
+# generate input vector for generator
 def generate_input_vector(nb_samples, input_dim = 100):
-	norm_input = np.random.randn(input_dim * nb_samples)
-	norm_input = norm_input.reshape(nb_samples, input_dim)
+	norm_input = tf.random.normal([nb_samples, 100])
 	return norm_input
-
-# create fake samples
-def generate_fake_samples(g_model, nb_samples, input_dim = 100):
-	input_vector = generate_input_vector(nb_samples, input_dim)
-	g_images = g_model(input_vector)
-	is_real = np.zeros((nb_samples, 1))
-	return g_images, is_real
 
 #Functional API
 def make_generator_model(latent_dim = 100):
@@ -97,15 +80,6 @@ def sup_loss(y_true, y_pred):
 	cce_loss = tf.keras.losses.SparseCategoricalCrossentropy(from_logits = False)
 	return cce_loss(y_true, y_pred)
 
-def unsup_loss(y_true, y_pred):
-	sigmoid_loss = tf.keras.losses.BinaryCrossentropy(from_logits = False)
-	return sigmoid_loss(y_true, y_pred)
-
-def gen_loss(y_true, y_pred):
-	sigmoid_loss = tf.keras.losses.BinaryCrossentropy(from_logits = False)
-	return sigmoid_loss(y_true, y_pred)
-
-
 def discriminator_loss(real_output, fake_output):
 	cross_entropy = tf.keras.losses.BinaryCrossentropy(from_logits = True)
 	real_loss = cross_entropy(tf.ones_like(real_output), real_output)
@@ -117,8 +91,8 @@ def generator_loss(fake_output):
 	cross_entropy = tf.keras.losses.BinaryCrossentropy(from_logits = True)
 	return cross_entropy(tf.ones_like(fake_output), fake_output)
 
-
-def train_sup_step(X, y, s_model, s_opt, class_loss, class_acc):
+@tf.function
+def train_sup_step(X, y, s_model, s_opt, class_loss_metric, class_acc_metric):
 	with tf.GradientTape() as s_tape:
 		sup_preds = s_model(X, training = True)
 		supervised_loss = sup_loss(y, sup_preds)
@@ -126,10 +100,13 @@ def train_sup_step(X, y, s_model, s_opt, class_loss, class_acc):
 	supervised_grads = s_tape.gradient(supervised_loss, s_model.trainable_variables)
 	s_opt.apply_gradients(zip(supervised_grads, s_model.trainable_variables))
 
-	class_loss(supervised_loss)
-	class_acc(y, sup_preds)
+	class_loss_metric(supervised_loss)
+	class_acc_metric(y, sup_preds)
 
-def train_unsup_step(X_real, X_gen, d_model, d_opt, disc_loss_metric, g_model, g_opt, gen_loss_metric):
+@tf.function
+def train_unsup_step(X_real, batch_size, gen_dim, d_model, d_opt, disc_loss_metric, g_model, g_opt, gen_loss_metric):
+	X_gen = generate_input_vector(batch_size, gen_dim)
+
 	with tf.GradientTape() as gen_tape, tf.GradientTape() as disc_tape:
 		gen_images = g_model(X_gen, training = True)
 
@@ -137,7 +114,7 @@ def train_unsup_step(X_real, X_gen, d_model, d_opt, disc_loss_metric, g_model, g
 		fake_output = d_model(gen_images, training = True)
 
 		gen_loss = generator_loss(fake_output)
-		disc_loss = unsup_loss(real_output, fake_output)
+		disc_loss = discriminator_loss(real_output, fake_output)
 
 	gen_grads = gen_tape.gradient(gen_loss, g_model.trainable_variables)
 	disc_grads = disc_tape.gradient(disc_loss, d_model.trainable_variables)
@@ -148,19 +125,17 @@ def train_unsup_step(X_real, X_gen, d_model, d_opt, disc_loss_metric, g_model, g
 	disc_loss_metric(disc_loss)
 	gen_loss_metric(gen_loss)
 
-# def train_gen_step(X, y, d_model, g_model, g_opt, gen_loss):
+@tf.function
+def test_step(X, y, s_model, test_loss_metric, test_acc_metric):
+	test_preds = s_model(X, training = False)
+	test_loss = sup_loss(y, test_preds)
 
-# 	with tf.GradientTape() as g_tape:
-# 		gen_images = g_model(X, training = True)
-# 		disc_preds = d_model(gen_images, training = False)
-# 		generator_loss = gen_loss(y, disc_preds)
-	
-# 	generator_grads = g_tape.gradient(generator_loss, g_model.trainable_variables)
-# 	g_opt.apply_gradients(zip(generator_grads, g_model.trainable_variables))
-
-# 	gen_loss(generator_loss)
+	test_loss_metric(test_loss)
+	test_acc_metric(y, test_preds)
 
 
+nb_samples = 1000
+nb_classes = 10
 batch_size = 100
 gen_dim = 100
 nb_epochs = 20
@@ -169,12 +144,10 @@ nb_epochs = 20
 
 X_train = X_train.reshape(X_train.shape[0], X_train.shape[1], X_train.shape[2], -1).astype('float32')
 X_train = (X_train - 127.5) / 127.5
+X_test = X_test.reshape(X_test.shape[0], X_test.shape[1], X_test.shape[2], -1).astype('float32')
+X_test = (X_test - 127.5) / 127.5
 
 
-batches_per_epoch = X_train.shape[0] // batch_size
-nb_steps = batches_per_epoch * nb_epochs
-
-half_batch_size = batch_size // 2
 s_model, d_model = make_discriminator_models()
 s_opt = tf.keras.optimizers.Adam(lr = 0.0002, beta_1 = 0.5)
 d_opt = tf.keras.optimizers.Adam(lr = 0.0002, beta_1 = 0.5)
@@ -188,32 +161,51 @@ class_acc_metric = tf.keras.metrics.SparseCategoricalAccuracy(name = 'classifica
 disc_loss_metric = tf.keras.metrics.Mean(name = 'discriminator_loss')
 gen_loss_metric = tf.keras.metrics.Mean(name = 'generator_loss')
 
-for i in range(1, nb_steps + 1):
+test_loss_metric = tf.keras.metrics.Mean(name = 'test_loss')
+test_acc_metric = tf.keras.metrics.SparseCategoricalAccuracy(name = 'test_accuracy')
+
+
+
+
+X_sup, y_sup = select_supervised_samples(X_train, y_train, nb_samples, nb_classes)
+supervised_ds = tf.data.Dataset.from_tensor_slices((X_sup, y_sup)).shuffle(nb_samples).batch(batch_size)
+
+X_unsup, y_unsup = select_supervised_samples(X_train, y_train, nb_samples, nb_classes)
+unsupervised_ds = tf.data.Dataset.from_tensor_slices((X_unsup, y_unsup)).shuffle(nb_samples).batch(batch_size)
+
+test_ds = tf.data.Dataset.from_tensor_slices((X_test, y_test)).batch(batch_size)
+
+
+
+for epoch in range(1, nb_epochs + 1):
 
 	class_loss_metric.reset_states()
 	class_acc_metric.reset_states()
 	disc_loss_metric.reset_states()
 	gen_loss_metric.reset_states()
+	test_loss_metric.reset_states()
+	test_acc_metric.reset_states()
 
-	#Train s_model (softmax)
-	X_sup, y_sup = select_supervised_samples(X_train, y_train)
-	[X_sup_batch, y_sup_classes], _ = generate_real_samples(X_sup, y_sup, half_batch_size)
-	train_sup_step(X_sup_batch, y_sup_classes, s_model, s_opt, class_loss_metric, class_acc_metric)
+	# Train s_model (softmax)
+	for sup_images, sup_labels in supervised_ds:
+		train_sup_step(sup_images, sup_labels, s_model, s_opt, class_loss_metric, class_acc_metric)
 
-	#Train d_model/g_model (discriminator/generator)
-	[X_unsup_real, _], y_unsup_real = generate_real_samples(X_train, y_train, half_batch_size)
+	# Train d_model/g_model (discriminator/generator)
+	for unsup_images, unsup_labels in unsupervised_ds:
+		train_unsup_step(unsup_images, half_batch_size, gen_dim, d_model, d_opt, disc_loss_metric, g_model, g_opt, gen_loss_metric)
 
-	X_gen = generate_input_vector(half_batch_size, gen_dim)
+	for test_images, test_labels in test_ds:
+		test_step(test_images, test_labels, s_model, test_loss_metric, test_acc_metric)
 
-	# X_gen, y_gen = generate_input_vector(batch_size, gen_dim), tf.ones(batch_size, 1)
-	train_unsup_step(X_unsup_real, X_gen, d_model, d_opt, disc_loss_metric, g_model, g_opt, gen_loss_metric)
 
-	template = 'Step {}, Classification Loss: {}, Classification Accuracy: {}, Discriminator Loss: {}, Generator Loss: {}'
-	print(template.format(i,
+	template = 'Epoch {}, Classification Loss: {}, Classification Accuracy: {}, Discriminator Loss: {}, Generator Loss: {}, Test Loss {}, Test Accuracy: {}'
+	print(template.format(epoch,
 			class_loss_metric.result(),
 			class_acc_metric.result() * 100,
 			disc_loss_metric.result(),
-			gen_loss_metric.result()))
+			gen_loss_metric.result(),
+			test_loss_metric.result(),
+			test_acc_metric.result() * 100))
 
 
 
